07c3e524f0777b4b27c5dc7a321679addffb9576
Eduardo Mourão <edumouraoaragao@gmail.com>
2026-08-26 23:00:23 -0300

    Revert "feat: registra histórico de preços"

--- src/main/modules/rotas.module.ts
+++ src/main/modules/rotas.module.ts
@@ -3,7 +3,6 @@ import { PriceCheckJob } from '../../application/rotas/jobs/price-check.job';
 import { CriarRotaUseCase } from '../../application/rotas/use-cases/criar-rota.use-case';
 import { ListarHistoricoRotaUseCase } from '../../application/rotas/use-cases/listar-historico-rota.use-case';
 import { ListarRotasUseCase } from '../../application/rotas/use-cases/listar-rotas.use-case';
-import { RegistrarHistoricoPrecoUseCase } from '../../application/rotas/use-cases/registrar-historico-preco.use-case';
 import { ROTAS_REPOSITORY } from '../../domain/rotas/repositories/rotas.repository';
 import { PrismaRotasRepository } from '../../infra/database/prisma/repositories/prisma-rotas.repository';
 import { RotasController } from '../../infra/http/controllers/rotas.controller';
@@ -14,7 +13,6 @@ import { RotasController } from '../../infra/http/controllers/rotas.controller';
     CriarRotaUseCase,
     ListarRotasUseCase,
     ListarHistoricoRotaUseCase,
-    RegistrarHistoricoPrecoUseCase,
     PrismaRotasRepository,
     {
       provide: ROTAS_REPOSITORY,

6bf047c621c1f5b94b29d75918521ca1db6abb4b
Eduardo_Mourao <edumouraoaragao@gmail.com>
2026-08-27 16:39:25 -0300

    feat: adiciona autenticação OAuth2 da Amadeus

--- src/infra/amadeus/amadeus.module.ts
+++ src/infra/amadeus/amadeus.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AmadeusService } from './amadeus.service';
 
 @Module({
+  imports: [HttpModule],
   providers: [AmadeusService],
   exports: [AmadeusService],
 })
